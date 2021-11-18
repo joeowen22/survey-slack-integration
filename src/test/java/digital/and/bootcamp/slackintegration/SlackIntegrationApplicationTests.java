3358b75a09e75e6d4fc9cd55fc692357154ba44e
Joseph Owen <josephowen@Josephs-MacBook-Pro.local>
2021-11-18 14:16:46 +0000

    Template for spring

---
+++ src/test/java/digital/and/bootcamp/slackintegration/SlackIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package digital.and.bootcamp.slackintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlackIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
